9e78616f06e93c3796344bbd074013fba5462aa3
Jocelyn Maynard <joshembro@htmail.com>
2021-03-13 12:39:14 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Jocelyn Maymard on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
